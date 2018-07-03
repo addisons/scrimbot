8472d5bc8c7f2314bf6d4baecec7c8e505186af0
Unknown <addisonsf.personal@gmail.com>
2018-07-03 22:11:28 +1000

    Removed ~help function.

--- main.js
+++ main.js
@@ -300,35 +300,7 @@ client.on("message", async message => {
             }
         }
     }
-
-    // if (command === "help") {
-    //     const m = message.channel.send({embed: {
-    //             color: 3447003,
-    //             author: client.user.username,
-    //             title: "# This is an embed",
-    //             url: "http://google.com",
-    //             description: "This is a test embed to showcase what they look like and what they can do.",
-    //             fields: [{
-    //                 name: "Fields",
-    //                 value: "They can have different fields with small headlines."
-    //             },
-    //                 {
-    //                     name: "Masked links",
-    //                     value: "You can put [masked links](http://google.com) inside of rich embeds."
-    //                 },
-    //                 {
-    //                     name: "Markdown",
-    //                     value: "You can put all the *usual* **__Markdown__** inside of them."
-    //                 }
-    //             ],
-    //             timestamp: new Date(),
-    //             footer: {
-    //                 icon_url: client.user.avatarURL,
-    //                 text: "© Example"
-    //             }
-    //         }});
-    // }
-
+    
     if (command === "clear") {
         message.channel.bulkDelete(100);
     }
